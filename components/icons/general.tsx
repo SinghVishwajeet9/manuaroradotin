40ad7e2b63ecce39fc058c3978594f894ff316c3
vishwajeet <vishwajeetampaul@gmail.com>
2026-09-21 12:49:18 +0530

    fix: center Cursor icon in companies section
    The CursorIcon's viewBox (0 0 471 491) didn't match the actual bounding
    box of its path (x: 23-475, y: 18-528), causing the icon to render
    visually off-center within its square container. Adjusted the viewBox
    origin to align with the path's true center while keeping the same
    dimensions to preserve the icon's size.

--- components/icons/general.tsx
+++ components/icons/general.tsx
@@ -2,7 +2,7 @@ export const CursorIcon = (props: React.SVGProps<SVGSVGElement>) => {
   return (
     <svg
       fill="none"
-      viewBox="0 0 471 491"
+      viewBox="13.7 27.375 471 491"
       xmlns="http://www.w3.org/2000/svg"
       overflow="visible"
       {...props}
